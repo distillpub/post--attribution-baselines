7f72699cb52a4f4174f1a98033cf0b9a9d09e438
Pascal Sturmfels <psturm@cs.washington.edu>
2019-11-05 12:00:46 -0800

    continued work on decoy stuff

--- docs/data_gen/decoy_mnist_examples/data.py
+++ docs/data_gen/decoy_mnist_examples/data.py
@@ -8,7 +8,12 @@ def decoy_mnist():
 
     x_train = np.expand_dims(x_train, axis=-1)
     x_test  = np.expand_dims(x_test, axis=-1)
-    
+
+    # These two lines add the "decoy" part. We are literally
+    # encoding the true label into the top left corner of the image.
+    x_train[:, :3, :3, :] = y_train[:, np.newaxis, np.newaxis, np.newaxis]
+    x_test[:, :3, :3, :]  = y_test[:, np.newaxis, np.newaxis, np.newaxis]
+
     x_train = x_train * (1. / 255) - 0.5
     x_test  = x_test  * (1. / 255) - 0.5
     return x_train, y_train, x_test, y_test
